a4b1fd459e967a62b8a1286001da9182c7a159ba
DaFunEDeveloper <62800011+DaFunEDeveloper@users.noreply.github.com>
2020-03-29 13:26:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var noOne = prompt('Name the first number');
-var noTwo = prompt ('Name the second number');
-var finNo = noOne * noTwo;
-alert('The product is ' + finNo);
+var operation = prompt('What operation would you like to do? (please answer in lower case)')
 
-console.log
+if(operation == 'multiplication') {
+	var noOne = prompt('Name the first number');
+	var noTwo = prompt ('Name the second number');
+	var finNo = noOne * noTwo;
+	alert('The product is ' + finNo);
+}
+if(operation == 'division') {
+	var noOne = prompt('Name the first number');
+	var noTwo = prompt ('Name the second number');
+	var finNo = noOne / noTwo;
+	alert('The quotient is ' + finNo);
+}
+if(operation == 'subtraction') {
+	var noOne = prompt('Name the first number');
+	var noTwo = prompt ('Name the second number');
+	var finNo = noOne - noTwo;
+	alert('The difference is ' + finNo);
+}
+if(operation == 'addition') {
+	var noOne = prompt('Name the first number');
+	var noTwo = prompt ('Name the second number');
+	var negOne = 0 - noOne;
+	var finNo = noTwo - negOne;
+	alert('The sum is ' + finNo);
+}
