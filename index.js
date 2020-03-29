7d5292b559dabf1c907c21c3c2a8710925cc6b91
DaFunEDeveloper <62800011+DaFunEDeveloper@users.noreply.github.com>
2020-03-29 13:11:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var noOne = prompt('Name the first number')
-var noTwo = prompt ('Name the second number')
+var noOne = prompt('Name the first number');
+var noTwo = prompt ('Name the second number');
+var finNo = noOne * noTwo;
+alert('The product is ' + finNo);
+
+console.log
